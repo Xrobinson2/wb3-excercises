43524afe9f0cef372a84f15074a7e082cdc51d46
Xavier Robinson <x.robinson5041@outlook.com>
2024-05-02 10:07:46 -0400

    parsings

---
+++ Strings/parsing.js
@@ -0,0 +1,45 @@
+"use strict";
+
+
+
+let name = "Brenda Kaye";
+
+
+
+function indexOf(name) {
+  let firstName = name.substring(0, 6);
+  let lastName = name.substring(7, 11);
+
+  let message1 = " Name: " + name;
+  let message2 = " First Name: " + firstName;
+  let message3 = " Last Name: " + lastName;
+
+  console.log(message1);
+  console.log(message2);
+  console.log(message3);
+
+  let nameDigitString = firstName + lastName;
+  return nameDigitString;
+}
+
+let nameWithoutSpace = indexOf(name);
+console.log(nameWithoutSpace);
+
+
+
+function parseAndDisplayName() {
+  
+
+  let firstPart = name.substring(0, 6);
+  let secondPart = name.substring(7, 11);
+
+  let nameDigitString = firstName + lastName;
+  return nameDigitString;
+}
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
+
+let showNameAndNumber = parseAndDisplayName()
+console.log(showNameAndNumber)
