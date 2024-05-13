4e7569d802e3568255daf459673335040c7b81fa
Xavier Robinson <x.robinson5041@outlook.com>
2024-05-13 09:08:44 -0400

    part codes

--- Strings/part_codes.js
+++ Strings/part_codes.js
@@ -4,16 +4,19 @@
 
 
 
+
 let productCode0 = "ACME:123-L";
 let productCode1 = "DI:12345-M";
 let productCode2 = "ACE:1-12";
 
-//returns all characters before the :
+
 function getSupplier(code){
     return code.substring(0,code.indexOf(":"));
     }
 
-//returns all characters between : and -
+
+
+
 function getProductNumber(code) {
     return code.substring(code.indexOf(":") + 1, code.indexOf("-"));
 }
@@ -24,14 +27,14 @@ function getSize(code) {
 }
 
 
-//outputs the parts info in a concise way based on the earlier functions
+
 function outputPartInfo(code) {
     let supplier = getSupplier(code);
     let productNumber = getProductNumber(code);
     let size = getSize(code);
     let sizeExpanded;
 
-    //If-Else block that checks if the size is in SML format or is a number.
+    
     if (isNaN(Number(size))) {
         if (size.toUpperCase() == "L") {
             sizeExpanded = "large (L)";
